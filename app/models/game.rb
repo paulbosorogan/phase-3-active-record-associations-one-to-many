e7e1849ad3a30d9e7c97e093bedf3528668d1388
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-05-07 12:26:35 -0400

    commit tree 89ccc1fb4c7471edafe07c56136682083b6dd9bf

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  
+    belongs_to(:game)
+    has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
-  
+    belongs_to :game
+    def reviews
+        Review.where(game_id: self.id)
+      end
 end
